89874501c406996c35ad2dd09034f5a1b849acbe
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2019-01-08 11:55:16 -0600

    added login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 router.post('/register', async (req, res) => {
-  console.log(req.body, ' this is session')
+  console.log(req.body, ' this is register')
 
   try {
     const password = req.body.password;
@@ -21,11 +21,12 @@ router.post('/register', async (req, res) => {
 
     req.session.logged = true;
     req.session.username = req.body.username;
+    req.session.save();
 
 
     res.json({
       status: 200,
-      data: 'login successful'
+      data: 'registration successful'
     });
 
   } catch(err) {
@@ -33,6 +34,48 @@ router.post('/register', async (req, res) => {
   }
 
 });
+
+router.post('/login', async (req, res) => {
+  console.log(req.body, 'this is login');
+
+  try {
+
+    const foundUser = await User.findOne({'username': req.body.username});
+
+    if (foundUser) {
+
+      if ((bcrypt.compareSync(req.body.password, foundUser.password))) {
+
+        req.session.username = req.body.username;
+        req.session.logged = true;
+        req.session.save();
+        console.log(req.session);
+
+        res.json({
+          status: 200,
+          data: 'login successful'
+        });
+
+
+      } else {
+        console.log('login unsuccessful');
+        res.json({
+          status: 200,
+          data: 'login unsuccessful'
+        });
+      }
+    } else {
+      console.log('login unsuccessful');
+      res.json({
+        status: 200,
+        data: 'login unsuccessful'
+      });
+    }
+
+  } catch(err) {
+    console.log(err);
+  }
+})
 
 
 
