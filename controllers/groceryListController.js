4932ec935aba431c3181c61f8f3d3929e955c5ec
Christine Samarchi <csamarchi@gmail.com>
2019-01-08 15:40:06 -0600

    added route to get id

--- controllers/groceryListController.js
+++ controllers/groceryListController.js
@@ -3,15 +3,16 @@ const router = express.Router();
 const GroceryList = require('../models/groceryList');
 
 router.post('/create', async (req, res) => {
+  console.log(req.session);
   try {
 
     if (req.session.logged) {
       req.body.createdBy = req.session.username;
       const createdList = await GroceryList.create(req.body);
-
+  console.log(createdList);
       res.json({
         status: 200,
-        data: 'created list'
+        data: createdList._id
       })
     } else {
       res.json({
@@ -21,6 +22,43 @@ router.post('/create', async (req, res) => {
     }
 
   } catch (err) {
+    console.log(err);
+  }
+})
+
+router.get('/findLists', async (req, res) => {
+  console.log('it wrks        s')
+  try {
+
+    if (req.session.logged) {
+
+      const foundLists = await GroceryList.find({'createdBy': req.session.username});
+      console.log(foundLists);
+      res.json({
+        status: 200,
+        data: 'found lists'
+      })
+    } else {
+      res.json({
+        status: 200,
+        data: 'Log in required'
+      })
+    }
+
+  } catch (err) {
+    console.log(err);
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  console.log(req.params.id, 'get list request');
+  try {
+    const getList = await GroceryList.findById(req.params.id)
+    res.json({
+      status: 200,
+      data: getList,
+    })
+  } catch(err) {
     console.log(err);
   }
 })
