d0474cb610cfeaaf3440e2e14d2dbafcbfac0c48
Mirza Baig <mirzabaig@mirzas-mbp.lan>
2019-09-10 13:23:50 -0500

    pulling

--- models/groceryList.js
+++ models/groceryList.js
@@ -2,16 +2,7 @@ const mongoose = require('mongoose');
 
 const GroceryListSchema = new mongoose.Schema({
   name: String,
-  produce: [],
-  meats: [],
-  deli: [],
-  dairy: [],
-  frozen: [],
-  bakery: [],
-  general: [],
-  alcohol: [],
-  drinks: [],
-  dryGoods: [],
+  category: [],
   createdBy: String,
   color: String
 });
